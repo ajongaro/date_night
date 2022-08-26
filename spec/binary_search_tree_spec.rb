615eab816487115ef8843005246cbdba680386bf
Anthony Ongaro <aongaro@gmail.com>
2022-08-25 18:19:57 -0700

    feat: test lower depth placements in bst

--- spec/binary_search_tree_spec.rb
+++ spec/binary_search_tree_spec.rb
@@ -17,11 +17,17 @@ RSpec.describe BinarySearchTree do
       expect(tree.root).to be_a(TreeNode)
     end
 
-    it 'adds a score and title' do
+    it 'adds a score and title for root node' do
       tree.insert(50, 'Hannibal Buress: Animal Furnace')
 
       expect(tree.root.score).to eq(50)
       expect(tree.root.title).to eq('Hannibal Buress: Animal Furnace')
+    end
+
+    it 'has a size of one with one node' do
+      tree.insert(50, 'Hannibal Buress: Animal Furnace')
+
+      expect(tree.size).to eq(1)
     end
 
     it 'adds two scores and titles' do
@@ -35,6 +41,27 @@ RSpec.describe BinarySearchTree do
       expect(tree.root.right.title).to eq("Bill & Ted's Excellent Adventure")
       expect(tree.root.left).to eq(nil)
     end
-  end
 
+    it 'adds a lesser score title in left node of root' do
+      tree.insert(50, 'Hannibal Buress: Animal Furnace')
+      expect(tree.root.score).to eq(50)
+      expect(tree.root.title).to eq('Hannibal Buress: Animal Furnace')
+
+      tree.insert(32, 'Police Academy')
+      expect(tree.root.left.score).to eq(32)
+      expect(tree.root.left.title).to eq('Police Academy')
+    end
+
+    it 'has a depth of 2 when appropriate' do
+      tree.insert(50, 'Hannibal')
+      tree.insert(32, 'Police Academy')
+      tree.insert(10, 'GIGLi')
+
+      expect(tree.depth).to eq(2)
+      expect(tree.size).to eq(3)
+    end
+
+
+
+  end
 end
